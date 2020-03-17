3bd12f74c78ef64a7253c08f251e9ee5a5c0eeec
Paddy McGlone <paddymcglone@gmail.com>
2020-03-17 15:52:01 +0000

    Adding the foundations of the MVC app.

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Get items from the database
+
+            // Add items to the model
+
+            // Display the view using the model
+        }
+    }
+}
